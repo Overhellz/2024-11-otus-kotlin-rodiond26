d7a43dcfbf4cf33ddfe0e004f7d02c02d72ce73c
rodiond26 <rodiond26@gmail.com>
2024-12-01 16:54:11 +0500

    HW-01 добавил ДЗ1

---
+++ settings.gradle.kts
@@ -0,0 +1,14 @@
+pluginManagement {
+    plugins {
+        val kotlinVersion: String by settings
+        kotlin("jvm") version kotlinVersion
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
+
+rootProject.name = "2024-11-otus-kotlin-rodiond26"
+
+include("m1l1-first")
